6c952f226626715f5ea5a49d64f44ed6923b4963
GalaKtiKos2004 <94827294+GalaKtiKos2004@users.noreply.github.com>
2025-07-13 03:18:08 +1000

    Created ResourcePool

--- Assets/Scripts/ResourceGenerator.cs
+++ Assets/Scripts/ResourceGenerator.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 using Random = UnityEngine.Random;
 
 public class ResourceGenerator : MonoBehaviour
@@ -9,13 +9,25 @@ public class ResourceGenerator : MonoBehaviour
     [SerializeField] private List<SpawnPoint> _spawnPoints;
     [SerializeField] private Resource _prefab;
 
+    [SerializeField] private int _poolCapcity = 10;
+    [SerializeField] private int _maxPoolSize = 10;
     [SerializeField] private float _spawnDelay;
 
     private WaitForSeconds _wait;
     private List<SpawnPoint> _occupiedPoints = new List<SpawnPoint>();
+    private ObjectPool<Resource> _resources;
 
     private void Awake()
     {
+        _resources = new ObjectPool<Resource>(
+            createFunc: () => Instantiate(_prefab),
+            actionOnGet: resource => ActionOnGet(resource),
+            actionOnRelease: resource => resource.gameObject.SetActive(false),
+            actionOnDestroy: resource => Destroy(resource.gameObject),
+            collectionCheck: true,
+            defaultCapacity: _poolCapcity,
+            maxSize: _maxPoolSize);
+        
         _wait = new(_spawnDelay);
     }
 
@@ -37,6 +49,19 @@ public class ResourceGenerator : MonoBehaviour
         }
     }
 
+    private void ActionOnGet(Resource resource)
+    {
+        SpawnPoint spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Count)];
+
+        if (_occupiedPoints.Contains(spawnPoint))
+        {
+            return;
+        }
+        
+        resource.transform.position = spawnPoint.transform.position;
+        resource.gameObject.SetActive(true);
+    }
+
     private void OnResourceLeft(SpawnPoint spawnPoint) =>
         _occupiedPoints.Remove(spawnPoint);
 
@@ -44,14 +69,7 @@ public class ResourceGenerator : MonoBehaviour
     {
         while (enabled)
         {
-            SpawnPoint spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Count)];
-
-            if (_occupiedPoints.Contains(spawnPoint) == false)
-            {
-                spawnPoint.SpawnResource(_prefab);
-                _occupiedPoints.Add(spawnPoint);
-            }
-
+            _resources.Get();
             yield return _wait;
         }
     }
